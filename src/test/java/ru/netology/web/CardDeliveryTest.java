f8dd64bdb1d60f58994ab367ed352b976096c910
ITvladislavovna <irichka-k@rambler.ru>
2022-10-24 19:18:48 +0300

    DeliveryTest

--- src/test/java/ru/netology/web/CardDeliveryTest.java
+++ src/test/java/ru/netology/web/CardDeliveryTest.java
@@ -1,46 +1,32 @@
 package ru.netology.web;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+
 
 public class CardDeliveryTest {
 
-    private WebDriver  driver;
-
-
-    @BeforeAll
-    static void setUpAll() {
-
-        WebDriverManager.chromedriver().setup();
-    }
-
-    @BeforeEach
-    void setUp() {
-        ChromeOptions options = new ChromeOptions();
-        options.setHeadless(true);
-        driver = new ChromeDriver(options);
-    }
-
-    @AfterEach
-    void tearsDown() {
-        driver.quit();
-        driver = null;
+    String generateDate(int days, int month, int year) {
+        return LocalDate.now().plusDays(days).plusMonths(month).plusYears(year).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
 
     @Test
-    void shouldTest() {
-        driver.get("http://localhost:9999/");
-
-    }
-
-    private static class WebDriverManager {
-    }
-
-    private class WebDriver {
-    }
-
-    private class ChromeOptions {
+    void shouldRegisterByAccount() {
+        String date = generateDate(1, 8, 1);
+        open("http://localhost:9999/");
+        $("[data-test-id=city] input").setValue("Чебоксары");
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(date);
+        $("[data-test-id=name] input").setValue("Иванов Петр");
+        $("[data-test-id=phone] input").setValue("+79023278228");
+        $("[data-test-id=agreement]").click();
+        $$("[type=button]").find(exactText("Забронировать")).click();
+        $("[data-test-id=notification]").shouldHave(exactText("Успешно!\nВстреча успешно забронирована на " + date), Duration.ofMillis(15000)).shouldBe(exist);
     }
 }
